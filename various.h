03f8d2e79529f5152044512f4fe329723c1b5f68
Markus Uhlin <markus.uhlin@bredband.net>
2016-01-21 15:00:21 +0100

    Added function declarations for duc_strlcpy() and duc_strlcat().

--- various.h
+++ various.h
@@ -4,12 +4,17 @@
 #include <stdbool.h>
 #include <string.h> /* strcmp */
 
+#include "def.h" /* PTR_ARGS_NONNULL etc */
+
 size_t  size_product   (const size_t elt_count, const size_t elt_size);
 bool    file_exists    (const char *path);
 bool    is_regularFile (const char *path);
 bool    is_numeric     (const char *);
 char   *trim           (char *);
 char   *Strtolower     (char *);
+
+size_t	duc_strlcpy (char *dst, const char *src, size_t dsize) PTR_ARGS_NONNULL;
+size_t	duc_strlcat (char *dst, const char *src, size_t dsize) PTR_ARGS_NONNULL;
 
 static inline bool
 Strings_match(const char *str1, const char *str2)
