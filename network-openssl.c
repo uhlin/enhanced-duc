1bbbbe0b16eb37213c219bb2a31014d821cfa84f
Markus Uhlin <markus.uhlin@bredband.net>
2016-01-23 19:10:23 +0100

    Use dev urandom for seeding the PRNG because it's non-blocking.

--- network-openssl.c
+++ network-openssl.c
@@ -33,7 +33,7 @@ net_ssl_init()
     SSL_load_error_strings();
     SSL_library_init();
 
-    if (RAND_load_file("/dev/random", 1024) <= 0) {
+    if (RAND_load_file("/dev/urandom", 1024) <= 0) {
 	log_warn(ENOSYS, "net_ssl_init: Error seeding the PRNG! LibreSSL?");
     }
     
