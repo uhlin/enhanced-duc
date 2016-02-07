6d0d62e8467acb759582db86908530d36e1def19
Markus Uhlin <markus.uhlin@bredband.net>
2016-02-07 15:17:36 +0100

    v1.4

--- main.c
+++ main.c
@@ -32,7 +32,7 @@
 #include "wrapper.h"
 
 const char g_programName[]     = "Enhanced DUC";
-const char g_programVersion[]  = "v1.3.2";
+const char g_programVersion[]  = "v1.4";
 const char g_programAuthor[]   = "Markus Uhlin";
 const char g_maintainerEmail[] = "markus.uhlin@bredband.net";
 
