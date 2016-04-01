d3069d2f45a649e94e99a4bc1c1bb3a81b2ad64e
Markus Uhlin <markus.uhlin@bredband.net>
2016-04-01 15:45:24 +0200

    More tests for the interpreter

--- config-test.sh
+++ config-test.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 PREFIX=tests/
-TEST_FILES="test1.conf test2.conf test3.conf"
+TEST_FILES="test1.conf test2.conf test3.conf test4.conf test5.conf test6.conf"
 
 for conf in $TEST_FILES; do
     echo "========== ${PREFIX}${conf} =========="
-    ./educ_noip -x ${PREFIX}${conf} -o
+
+    if test -f "${PREFIX}${conf}"; then
+	./educ_noip -x ${PREFIX}${conf} -o
+    else
+	echo "No output."
+	exit 1
+    fi
 done
 
 exit 0
