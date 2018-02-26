d58ecb04d5948dd41e7b512128216ebb2444f9d9
Markus Uhlin <markus.uhlin@bredband.net>
2018-02-26 23:03:27 +0100

    put type of free_not_null() on a separate line

--- wrapper.h
+++ wrapper.h
@@ -11,7 +11,8 @@ void *xrealloc      (void *ptr, size_t newSize);
 char *xstrdup       (const char *string);
 char *Strdup_printf (const char *format, ...) PRINTFLIKE(1);
 
-static inline void free_not_null(void *ptr)
+static inline void
+free_not_null(void *ptr)
 {
     if (ptr != NULL)
 	free(ptr);
