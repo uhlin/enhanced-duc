100029a75967811faf0e115808f7ca1d620c8397
Markus Uhlin <markus.uhlin@bredband.net>
2018-02-27 16:02:12 +0100

    sorted the functions

--- settings.c
+++ settings.c
@@ -74,55 +74,86 @@ static struct config_default_values_tag {
       "This setting should be set to YES if ip_addr is not equal to value WAN_address." },
 };
 
-static bool	is_setting_ok         (const char *value, enum setting_type);
-static bool	is_recognized_setting (const char *setting_name);
-static int	install_setting       (const char *setting_name, const char *value);
-static bool	is_ip_addr_ok         (char **reason);
-static bool	is_hostname_ok        (const char *host, char **reason);
-static bool	is_port_ok            (void);
-
 /**
- * @brief Creates a configuration file
- * @param path Specifies where to create the file including its filename
- * @return void
+ * @brief Unparse a setting of type boolean
+ * @param setting_name	Setting name
+ * @param fallback_val	Fallback value
+ * @return true or false
  *
- * Creates a configuration file for the DUC by asking the user for
- * input. On empty input (that is: if the user just hits ENTER) a
- * default value is used.
+ * Unparse a setting of type boolean. In either way: if a setting
+ * isn't found at all, or the setting found isn't of type boolean, it
+ * returns the fallback value.
  */
-void
-create_config_file(const char *path)
+bool
+setting_bool_unparse(const char *setting_name, const bool fallback_val)
 {
-    FILE		*fp   = NULL;
-    const mode_t	 mode = S_IRUSR | S_IWUSR;	/* Read and write for the owner. */
+    struct config_default_values_tag *cdv;
+    const size_t ar_sz = ARRAY_SIZE(config_default_values);
 
-    log_assert_arg_nonnull("create_config_file", "path", path);
+    if (setting_name == NULL) {
+	return (fallback_val);
+    }
 
-    if (file_exists(path)) {
-	fatal(EEXIST, "%s create_config_file: can't create config file  --  it's already existent", GfxFailure);
-    } else if ((fp = fopen(path, "w")) == NULL) {
-	fatal(errno, "%s create_config_file: fopen error", GfxFailure);
-    } else {
-	struct config_default_values_tag *cdv;
-	const size_t ar_sz = ARRAY_SIZE(config_default_values);
+    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
+	if (strings_match(setting_name, cdv->setting_name)) {
+	    const char *value = cdv->custom_val ? cdv->custom_val : cdv->value;
 
-	for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
-	    char *ans = get_answer(cdv->description, cdv->type, cdv->value);
+	    if (cdv->type != TYPE_BOOLEAN) {
+		log_warn(0, "setting_bool_unparse: %s: setting not a boolean!", setting_name);
+		break;
+	    } else if (strings_match(value, "yes") || strings_match(value, "YES")) {
+		return (true);
+	    } else if (strings_match(value, "no") || strings_match(value, "NO")) {
+		return (false);
+	    } else {
+		log_warn(0, "setting_bool_unparse: %s: setting has an invalid value!", setting_name);
+		break;
+	    }
+	} /* if */
+    } /* for */
 
-	    if (fprintf(fp, "%s = \"%s\";\n", cdv->setting_name, ans) < 0)
-		fatal(0, "%s create_config_file: failed to write to the file stream", GfxFailure);
+    return (fallback_val);
+}
 
-	    free(ans);
+static bool
+is_setting_ok(const char *value, enum setting_type type)
+{
+    if (value == NULL) {
+	return false;
+    }
+
+    switch (type) {
+    case TYPE_BOOLEAN:
+    {
+	if (!strings_match(value, "yes") && !strings_match(value, "YES") &&
+	    !strings_match(value, "no") && !strings_match(value, "NO")) {
+	    log_warn(0, "is_setting_ok: booleans must be either: yes, YES, no or NO");
+	    return false;
 	}
-
-	fclose(fp);
+	break;
+    }
+    case TYPE_INTEGER:
+    {
+	if (!is_numeric(value)) {
+	    log_warn(0, "is_setting_ok: integer not all numeric");
+	    return false;
+	}
+	break;
+    }
+    case TYPE_STRING:
+    {
+	if (strpbrk(value, " \f\n\r\t\v\"") != NULL) {
+	    log_warn(0, "is_setting_ok: illegal characters in string");
+	    return false;
+	}
+	break;
+    }
+    default:
+	assert(false); /* Should not be reached. */
+	break;
     }
 
-    if (chmod(path, mode) != 0) {
-	fatal(errno, "%s create_config_file: chmod error", GfxFailure);
-    }
-
-    printf("%s %s successfully written!\n", GfxSuccess, path);
+    return true;
 }
 
 /**
@@ -176,168 +207,6 @@ get_answer(const char *desc, enum setting_type type, const char *defaultAnswer)
 	exit(1);
 
     return (answer);
-}
-
-static bool
-is_setting_ok(const char *value, enum setting_type type)
-{
-    if (value == NULL) {
-	return false;
-    }
-
-    switch (type) {
-    case TYPE_BOOLEAN:
-    {
-	if (!strings_match(value, "yes") && !strings_match(value, "YES") &&
-	    !strings_match(value, "no") && !strings_match(value, "NO")) {
-	    log_warn(0, "is_setting_ok: booleans must be either: yes, YES, no or NO");
-	    return false;
-	}
-	break;
-    }
-    case TYPE_INTEGER:
-    {
-	if (!is_numeric(value)) {
-	    log_warn(0, "is_setting_ok: integer not all numeric");
-	    return false;
-	}
-	break;
-    }
-    case TYPE_STRING:
-    {
-	if (strpbrk(value, " \f\n\r\t\v\"") != NULL) {
-	    log_warn(0, "is_setting_ok: illegal characters in string");
-	    return false;
-	}
-	break;
-    }
-    default:
-	assert(false); /* Should not be reached. */
-	break;
-    }
-
-    return true;
-}
-
-/**
- * @brief Reads a configuration file
- * @param path Path to the file
- * @return void
- *
- * Reads a configuration file line by line, and installs settings,
- * until an end of file condition is entercounted.
- */
-void
-read_config_file(const char *path)
-{
-    FILE *fp = NULL;
-
-    log_assert_arg_nonnull("read_config_file", "path", path);
-
-    if (g_conf_read) {
-	return;
-    } else if (!is_regularFile(path)) {
-	fatal(0, "read_config_file: either the config file is nonexistent  --  or it isn't a regular file");
-    } else if ((fp = fopen(path, "r")) == NULL) {
-	fatal(errno, "read_config_file: fopen error");
-    } else {
-	char		buf[900];
-	long int	line_num = 0;
-
-	while (BZERO(buf, sizeof buf), fgets(buf, sizeof buf, fp) != NULL) {
-	    const char			*ccp	     = &buf[0];
-	    const char			 commentChar = '#';
-	    char			*line	     = NULL;
-	    struct Interpreter_in	 in;
-
-	    adv_while_isspace(&ccp);
-	    if (strings_match(ccp, "") || *ccp == commentChar) {
-		line_num++;
-		continue;
-	    }
-
-	    line = trim(xstrdup(ccp));
-	    in.path	      = (char *) path;
-	    in.line	      = line;
-	    in.line_num	      = ++line_num;
-	    in.validator_func = is_recognized_setting;
-	    in.install_func   = install_setting;
-	    Interpreter(&in);
-	    free_not_null(line);
-	}
-    }
-
-    if (feof(fp)) {
-	fclose(fp);
-    } else if (ferror(fp)) {
-	fatal(0, "read_config_file: FATAL: fgets returned null and the error indicator is set");
-    } else {
-	fatal(0, "read_config_file: FATAL: fgets returned null and the reason cannot be determined");
-    }
-
-    g_conf_read = true;
-}
-
-static bool
-is_recognized_setting(const char *setting_name)
-{
-    struct config_default_values_tag *cdv;
-    const size_t ar_sz = ARRAY_SIZE(config_default_values);
-
-    if (setting_name == NULL)
-	return false;
-
-    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
-	if (strings_match(setting_name, cdv->setting_name))
-	    return true;
-    }
-
-    return false;
-}
-
-static int
-install_setting(const char *setting_name, const char *value)
-{
-    struct config_default_values_tag *cdv;
-    const size_t ar_sz = ARRAY_SIZE(config_default_values);
-
-    if (setting_name == NULL || value == NULL)
-	return (EINVAL);
-
-    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
-	if (strings_match(setting_name, cdv->setting_name)) {
-	    if (cdv->custom_val) {
-		return (EBUSY);
-	    } else if (!is_setting_ok(value, cdv->type)) {
-		return (EINVAL);
-	    } else {
-		cdv->custom_val = xstrdup(value);
-		return (0);
-	    }
-	}
-    }
-
-    return (ENOENT);
-}
-
-/**
- * @brief	Free dynamically allocated memory
- * @return	void
- *
- * Settings that are customized are read into the memory. This
- * function destroys them.
- */
-void
-destroy_config_customValues(void)
-{
-    struct config_default_values_tag *cdv;
-    const size_t ar_sz = ARRAY_SIZE(config_default_values);
-
-    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++)
-	if (cdv->custom_val) {
-	    free(cdv->custom_val);
-	    cdv->custom_val = NULL;
-	}
 }
 
 /**
@@ -399,82 +268,6 @@ setting_integer_unparse(const struct integer_unparse_context *ctx)
     }
 
     return (ctx->fallback_val);
-}
-
-/**
- * @brief Unparse a setting of type boolean
- * @param setting_name	Setting name
- * @param fallback_val	Fallback value
- * @return true or false
- *
- * Unparse a setting of type boolean. In either way: if a setting
- * isn't found at all, or the setting found isn't of type boolean, it
- * returns the fallback value.
- */
-bool
-setting_bool_unparse(const char *setting_name, const bool fallback_val)
-{
-    struct config_default_values_tag *cdv;
-    const size_t ar_sz = ARRAY_SIZE(config_default_values);
-
-    if (setting_name == NULL) {
-	return (fallback_val);
-    }
-
-    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
-	if (strings_match(setting_name, cdv->setting_name)) {
-	    const char *value = cdv->custom_val ? cdv->custom_val : cdv->value;
-
-	    if (cdv->type != TYPE_BOOLEAN) {
-		log_warn(0, "setting_bool_unparse: %s: setting not a boolean!", setting_name);
-		break;
-	    } else if (strings_match(value, "yes") || strings_match(value, "YES")) {
-		return (true);
-	    } else if (strings_match(value, "no") || strings_match(value, "NO")) {
-		return (false);
-	    } else {
-		log_warn(0, "setting_bool_unparse: %s: setting has an invalid value!", setting_name);
-		break;
-	    }
-	} /* if */
-    } /* for */
-
-    return (fallback_val);
-}
-
-/**
- * @brief	Check some settings strictly
- * @return	void
- *
- * Validate that certain settings are OK.
- */
-void
-check_some_settings_strictly(void)
-{
-    const char *username = setting("username");
-    const char *password = setting("password");
-    const size_t username_maxlen = 50;
-    const size_t password_maxlen = 120;
-    char *reason = "";
-
-    if (strings_match(username, "") || strings_match(password, ""))
-	fatal(0, "error: empty username nor password");
-    else if (strlen(username) > username_maxlen)
-	fatal(0, "error: username too long. max=%zu", username_maxlen);
-    else if (strlen(password) > password_maxlen)
-	fatal(0, "error: password too long. max=%zu", password_maxlen);
-    else if (!is_ip_addr_ok(&reason))
-	fatal(0, "is_ip_addr_ok: error: %s", reason);
-    else if (!is_hostname_ok(setting("sp_hostname"), &reason))
-	fatal(0, "is_hostname_ok: sp_hostname: %s", reason);
-    else if (!is_port_ok())
-	fatal(0, "error: bogus port number");
-    else if (!is_hostname_ok(setting("primary_ip_lookup_srv"), &reason))
-	fatal(0, "is_hostname_ok: primary_ip_lookup_srv: %s", reason);
-    else if (!is_hostname_ok(setting("backup_ip_lookup_srv"), &reason))
-	fatal(0, "is_hostname_ok: backup_ip_lookup_srv: %s", reason);
-    else
-	return;
 }
 
 static bool
@@ -541,4 +334,204 @@ is_port_ok(void)
 	return false;
 
     /*NOTREACHED*/ return false;
+}
+
+/**
+ * @brief	Check some settings strictly
+ * @return	void
+ *
+ * Validate that certain settings are OK.
+ */
+void
+check_some_settings_strictly(void)
+{
+    const char *username = setting("username");
+    const char *password = setting("password");
+    const size_t username_maxlen = 50;
+    const size_t password_maxlen = 120;
+    char *reason = "";
+
+    if (strings_match(username, "") || strings_match(password, ""))
+	fatal(0, "error: empty username nor password");
+    else if (strlen(username) > username_maxlen)
+	fatal(0, "error: username too long. max=%zu", username_maxlen);
+    else if (strlen(password) > password_maxlen)
+	fatal(0, "error: password too long. max=%zu", password_maxlen);
+    else if (!is_ip_addr_ok(&reason))
+	fatal(0, "is_ip_addr_ok: error: %s", reason);
+    else if (!is_hostname_ok(setting("sp_hostname"), &reason))
+	fatal(0, "is_hostname_ok: sp_hostname: %s", reason);
+    else if (!is_port_ok())
+	fatal(0, "error: bogus port number");
+    else if (!is_hostname_ok(setting("primary_ip_lookup_srv"), &reason))
+	fatal(0, "is_hostname_ok: primary_ip_lookup_srv: %s", reason);
+    else if (!is_hostname_ok(setting("backup_ip_lookup_srv"), &reason))
+	fatal(0, "is_hostname_ok: backup_ip_lookup_srv: %s", reason);
+    else
+	return;
+}
+
+/**
+ * @brief Creates a configuration file
+ * @param path Specifies where to create the file including its filename
+ * @return void
+ *
+ * Creates a configuration file for the DUC by asking the user for
+ * input. On empty input (that is: if the user just hits ENTER) a
+ * default value is used.
+ */
+void
+create_config_file(const char *path)
+{
+    FILE		*fp   = NULL;
+    const mode_t	 mode = S_IRUSR | S_IWUSR;	/* Read and write for the owner. */
+
+    log_assert_arg_nonnull("create_config_file", "path", path);
+
+    if (file_exists(path)) {
+	fatal(EEXIST, "%s create_config_file: can't create config file  --  it's already existent", GfxFailure);
+    } else if ((fp = fopen(path, "w")) == NULL) {
+	fatal(errno, "%s create_config_file: fopen error", GfxFailure);
+    } else {
+	struct config_default_values_tag *cdv;
+	const size_t ar_sz = ARRAY_SIZE(config_default_values);
+
+	for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
+	    char *ans = get_answer(cdv->description, cdv->type, cdv->value);
+
+	    if (fprintf(fp, "%s = \"%s\";\n", cdv->setting_name, ans) < 0)
+		fatal(0, "%s create_config_file: failed to write to the file stream", GfxFailure);
+
+	    free(ans);
+	}
+
+	fclose(fp);
+    }
+
+    if (chmod(path, mode) != 0) {
+	fatal(errno, "%s create_config_file: chmod error", GfxFailure);
+    }
+
+    printf("%s %s successfully written!\n", GfxSuccess, path);
+}
+
+/**
+ * @brief	Free dynamically allocated memory
+ * @return	void
+ *
+ * Settings that are customized are read into the memory. This
+ * function destroys them.
+ */
+void
+destroy_config_customValues(void)
+{
+    struct config_default_values_tag *cdv;
+    const size_t ar_sz = ARRAY_SIZE(config_default_values);
+
+    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++)
+	if (cdv->custom_val) {
+	    free(cdv->custom_val);
+	    cdv->custom_val = NULL;
+	}
+}
+
+static bool
+is_recognized_setting(const char *setting_name)
+{
+    struct config_default_values_tag *cdv;
+    const size_t ar_sz = ARRAY_SIZE(config_default_values);
+
+    if (setting_name == NULL)
+	return false;
+
+    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
+	if (strings_match(setting_name, cdv->setting_name))
+	    return true;
+    }
+
+    return false;
+}
+
+static int
+install_setting(const char *setting_name, const char *value)
+{
+    struct config_default_values_tag *cdv;
+    const size_t ar_sz = ARRAY_SIZE(config_default_values);
+
+    if (setting_name == NULL || value == NULL)
+	return (EINVAL);
+
+    for (cdv = &config_default_values[0]; cdv < &config_default_values[ar_sz]; cdv++) {
+	if (strings_match(setting_name, cdv->setting_name)) {
+	    if (cdv->custom_val) {
+		return (EBUSY);
+	    } else if (!is_setting_ok(value, cdv->type)) {
+		return (EINVAL);
+	    } else {
+		cdv->custom_val = xstrdup(value);
+		return (0);
+	    }
+	}
+    }
+
+    return (ENOENT);
+}
+
+/**
+ * @brief Reads a configuration file
+ * @param path Path to the file
+ * @return void
+ *
+ * Reads a configuration file line by line, and installs settings,
+ * until an end of file condition is entercounted.
+ */
+void
+read_config_file(const char *path)
+{
+    FILE *fp = NULL;
+
+    log_assert_arg_nonnull("read_config_file", "path", path);
+
+    if (g_conf_read) {
+	return;
+    } else if (!is_regularFile(path)) {
+	fatal(0, "read_config_file: either the config file is nonexistent  --  or it isn't a regular file");
+    } else if ((fp = fopen(path, "r")) == NULL) {
+	fatal(errno, "read_config_file: fopen error");
+    } else {
+	char		buf[900];
+	long int	line_num = 0;
+
+	while (BZERO(buf, sizeof buf), fgets(buf, sizeof buf, fp) != NULL) {
+	    const char			*ccp	     = &buf[0];
+	    const char			 commentChar = '#';
+	    char			*line	     = NULL;
+	    struct Interpreter_in	 in;
+
+	    adv_while_isspace(&ccp);
+	    if (strings_match(ccp, "") || *ccp == commentChar) {
+		line_num++;
+		continue;
+	    }
+
+	    line = trim(xstrdup(ccp));
+	    in.path	      = (char *) path;
+	    in.line	      = line;
+	    in.line_num	      = ++line_num;
+	    in.validator_func = is_recognized_setting;
+	    in.install_func   = install_setting;
+	    Interpreter(&in);
+	    free_not_null(line);
+	}
+    }
+
+    if (feof(fp)) {
+	fclose(fp);
+    } else if (ferror(fp)) {
+	fatal(0, "read_config_file: FATAL: fgets returned null and the error indicator is set");
+    } else {
+	fatal(0, "read_config_file: FATAL: fgets returned null and the reason cannot be determined");
+    }
+
+    g_conf_read = true;
 }
