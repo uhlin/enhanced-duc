22a50ab8cf3a9401978ffb8cb66db9f8b96e71d2
Markus Uhlin <markus.uhlin@bredband.net>
2018-02-27 02:34:55 +0100

    enhanced duc config initial commit

---
+++ enhanced-duc-config.h
@@ -0,0 +1,10 @@
+#ifndef ENHANCED_DUC_CONFIG_H
+#define ENHANCED_DUC_CONFIG_H
+
+/* Set to 1 if your system has strlcpy() */
+#define HAVE_STRLCPY 0
+
+/* Set to 1 if your system has strlcat() */
+#define HAVE_STRLCAT 0
+
+#endif
