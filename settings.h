59829e531b025f63bef50411fb8c231c42acf0a7
Markus Uhlin <markus.uhlin@bredband.net>
2018-02-27 15:19:31 +0100

    sorted the function declarations

--- settings.h
+++ settings.h
@@ -12,13 +12,13 @@ struct integer_unparse_context {
 
 extern bool g_conf_read;
 
-void		 create_config_file           (const char *path);
+bool		 setting_bool_unparse         (const char *setting_name, const bool fallback_val);
 char		*get_answer                   (const char *desc, enum setting_type, const char *defaultAnswer);
-void		 read_config_file             (const char *path);
-void		 destroy_config_customValues  (void);
 const char	*setting                      (const char *setting_name);
 long int	 setting_integer_unparse      (const struct integer_unparse_context *);
-bool		 setting_bool_unparse         (const char *setting_name, const bool fallback_val);
 void		 check_some_settings_strictly (void);
+void		 create_config_file           (const char *path);
+void		 destroy_config_customValues  (void);
+void		 read_config_file             (const char *path);
 
 #endif
