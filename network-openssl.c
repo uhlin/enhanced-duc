72ffbd215c2a1af3b2847534fda77c0c281dead9
Markus Uhlin <markus.uhlin@bredband.net>
2018-03-02 00:15:03 +0100

    sorting of functions

--- network-openssl.c
+++ network-openssl.c
@@ -33,6 +33,165 @@ static SSL	*ssl	 = NULL;
 
 static const char cipher_list[] = "HIGH:!aNULL";
 
+/**
+ * Read bytes from a TLS/SSL connection
+ *
+ * @param recvbuf	Receive buffer
+ * @param recvbuf_size	Receive buffer size
+ * @return 0 on success, and -1 on failure
+ */
+int
+net_ssl_recv(char *recvbuf, size_t recvbuf_size)
+{
+    const int maxfdp1 = g_socket + 1;
+    fd_set readset;
+    struct timeval tv = {
+	.tv_sec  = 10,
+	.tv_usec = 0,
+    };
+
+    log_assert_arg_nonnull("net_ssl_recv", "recvbuf", recvbuf);
+
+    FD_ZERO(&readset);
+    FD_SET(g_socket, &readset);
+
+    errno = 0;
+
+    if (select(maxfdp1, &readset, NULL, NULL, &tv) == -1) {
+	log_warn(errno, "net_ssl_recv: select");
+	return -1;
+    } else if (!FD_ISSET(g_socket, &readset)) {
+	log_warn(0, "net_ssl_recv: no data to receive  --  timed out!");
+	return -1;
+    }
+
+    int bytes_received = 0;
+    ERR_clear_error();
+
+    if ((bytes_received = SSL_read(ssl, recvbuf, recvbuf_size)) > 0)
+	return 0;
+    switch (SSL_get_error(ssl, bytes_received)) {
+    case SSL_ERROR_NONE:
+	return 0;
+    case SSL_ERROR_WANT_READ:
+    case SSL_ERROR_WANT_WRITE:
+	log_warn(0, "net_ssl_recv: want read / want write");
+	return 0;
+    }
+
+    return -1;
+}
+
+/**
+ * Write bytes to a TLS/SSL connection
+ *
+ * @param fmt Format control
+ * @return 0 on success, and -1 on failure
+ */
+int
+net_ssl_send(const char *fmt, ...)
+{
+    char *buf = NULL;
+    extern int my_vasprintf(char **, const char *, va_list);
+    int buflen = 0;
+    int n_sent = 0;
+    size_t newSize = 0;
+    static const char message_terminate[] = "\r\n\r\n";
+    va_list ap = { 0 };
+
+    log_assert_arg_nonnull("net_ssl_send", "fmt", fmt);
+
+    va_start(ap, fmt);
+    if (my_vasprintf(&buf, fmt, ap) < 0)
+	fatal(errno, "net_ssl_send: my_vasprintf");
+    va_end(ap);
+
+    newSize = strlen(buf) + sizeof message_terminate;
+    buf = xrealloc(buf, newSize);
+
+    if (strlcat(buf, message_terminate, newSize) >= newSize)
+	fatal(EOVERFLOW, "net_ssl_send: strlcat");
+
+    if (strlen(buf) > INT_MAX) {
+	free(buf);
+	return -1;
+    }
+
+    buflen = (int) strlen(buf);
+    ERR_clear_error();
+
+    if ((n_sent = SSL_write(ssl, buf, buflen)) > 0) {
+	free(buf);
+	return 0;
+    }
+
+    free(buf);
+
+    switch (SSL_get_error(ssl, n_sent)) {
+    case SSL_ERROR_NONE:
+	return 0;
+    case SSL_ERROR_WANT_READ:
+    case SSL_ERROR_WANT_WRITE:
+	log_warn(0, "net_ssl_send: operation did not complete");
+	return 0;
+    }
+
+    return -1;
+}
+
+/**
+ * Initiate the TLS/SSL handshake with an TLS/SSL server
+ *
+ * @return 0 on success, -1 on error
+ */
+int
+net_ssl_start(void)
+{
+    const int VALUE_HANDSHAKE_OK = 1;
+
+    if ((ssl = SSL_new(ssl_ctx)) == NULL)
+	fatal(ENOMEM, "net_ssl_start: unable to create a new ssl object");
+    else if (!SSL_set_fd(ssl, g_socket))
+	log_warn(0, "net_ssl_start: "
+	    "unable to associate the global socket fd with the ssl object");
+    else if (SSL_connect(ssl) != VALUE_HANDSHAKE_OK)
+	log_warn(0, "net_ssl_start: handshake not ok!");
+    else
+	return (0);
+
+    return (-1);
+}
+
+/**
+ * Shut down a TLS/SSL connection
+ */
+void
+net_ssl_close(void)
+{
+    if (ssl) {
+	SSL_shutdown(ssl);
+	SSL_free(ssl);
+	ssl = NULL;
+    }
+}
+
+/**
+ * Deinitialize the TLS/SSL library
+ */
+void
+net_ssl_deinit()
+{
+    if (ssl) {
+	SSL_shutdown(ssl);
+	SSL_free(ssl);
+	ssl = NULL;
+    }
+    if (ssl_ctx) {
+	SSL_CTX_free(ssl_ctx);
+	ssl_ctx = NULL;
+    }
+}
+
 static int
 verify_callback(int ok, X509_STORE_CTX *ctx)
 {
@@ -103,163 +262,4 @@ net_ssl_init()
     net_recv = net_ssl_recv;
 
     log_msg("TLS/SSL enabled");
-}
-
-/**
- * Deinitialize the TLS/SSL library
- */
-void
-net_ssl_deinit()
-{
-    if (ssl) {
-	SSL_shutdown(ssl);
-	SSL_free(ssl);
-	ssl = NULL;
-    }
-    if (ssl_ctx) {
-	SSL_CTX_free(ssl_ctx);
-	ssl_ctx = NULL;
-    }
-}
-
-/**
- * Shut down a TLS/SSL connection
- */
-void
-net_ssl_close(void)
-{
-    if (ssl) {
-	SSL_shutdown(ssl);
-	SSL_free(ssl);
-	ssl = NULL;
-    }
-}
-
-/**
- * Initiate the TLS/SSL handshake with an TLS/SSL server
- *
- * @return 0 on success, -1 on error
- */
-int
-net_ssl_start(void)
-{
-    const int VALUE_HANDSHAKE_OK = 1;
-
-    if ((ssl = SSL_new(ssl_ctx)) == NULL)
-	fatal(ENOMEM, "net_ssl_start: unable to create a new ssl object");
-    else if (!SSL_set_fd(ssl, g_socket))
-	log_warn(0, "net_ssl_start: "
-	    "unable to associate the global socket fd with the ssl object");
-    else if (SSL_connect(ssl) != VALUE_HANDSHAKE_OK)
-	log_warn(0, "net_ssl_start: handshake not ok!");
-    else
-	return (0);
-
-    return (-1);
-}
-
-/**
- * Write bytes to a TLS/SSL connection
- *
- * @param fmt Format control
- * @return 0 on success, and -1 on failure
- */
-int
-net_ssl_send(const char *fmt, ...)
-{
-    char *buf = NULL;
-    extern int my_vasprintf(char **, const char *, va_list);
-    int buflen = 0;
-    int n_sent = 0;
-    size_t newSize = 0;
-    static const char message_terminate[] = "\r\n\r\n";
-    va_list ap = { 0 };
-
-    log_assert_arg_nonnull("net_ssl_send", "fmt", fmt);
-
-    va_start(ap, fmt);
-    if (my_vasprintf(&buf, fmt, ap) < 0)
-	fatal(errno, "net_ssl_send: my_vasprintf");
-    va_end(ap);
-
-    newSize = strlen(buf) + sizeof message_terminate;
-    buf = xrealloc(buf, newSize);
-
-    if (strlcat(buf, message_terminate, newSize) >= newSize)
-	fatal(EOVERFLOW, "net_ssl_send: strlcat");
-
-    if (strlen(buf) > INT_MAX) {
-	free(buf);
-	return -1;
-    }
-
-    buflen = (int) strlen(buf);
-    ERR_clear_error();
-
-    if ((n_sent = SSL_write(ssl, buf, buflen)) > 0) {
-	free(buf);
-	return 0;
-    }
-
-    free(buf);
-
-    switch (SSL_get_error(ssl, n_sent)) {
-    case SSL_ERROR_NONE:
-	return 0;
-    case SSL_ERROR_WANT_READ:
-    case SSL_ERROR_WANT_WRITE:
-	log_warn(0, "net_ssl_send: operation did not complete");
-	return 0;
-    }
-
-    return -1;
-}
-
-/**
- * Read bytes from a TLS/SSL connection
- *
- * @param recvbuf	Receive buffer
- * @param recvbuf_size	Receive buffer size
- * @return 0 on success, and -1 on failure
- */
-int
-net_ssl_recv(char *recvbuf, size_t recvbuf_size)
-{
-    const int maxfdp1 = g_socket + 1;
-    fd_set readset;
-    struct timeval tv = {
-	.tv_sec  = 10,
-	.tv_usec = 0,
-    };
-
-    log_assert_arg_nonnull("net_ssl_recv", "recvbuf", recvbuf);
-
-    FD_ZERO(&readset);
-    FD_SET(g_socket, &readset);
-
-    errno = 0;
-
-    if (select(maxfdp1, &readset, NULL, NULL, &tv) == -1) {
-	log_warn(errno, "net_ssl_recv: select");
-	return -1;
-    } else if (!FD_ISSET(g_socket, &readset)) {
-	log_warn(0, "net_ssl_recv: no data to receive  --  timed out!");
-	return -1;
-    }
-
-    int bytes_received = 0;
-    ERR_clear_error();
-
-    if ((bytes_received = SSL_read(ssl, recvbuf, recvbuf_size)) > 0)
-	return 0;
-    switch (SSL_get_error(ssl, bytes_received)) {
-    case SSL_ERROR_NONE:
-	return 0;
-    case SSL_ERROR_WANT_READ:
-    case SSL_ERROR_WANT_WRITE:
-	log_warn(0, "net_ssl_recv: want read / want write");
-	return 0;
-    }
-
-    return -1;
 }
