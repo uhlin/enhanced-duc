48c72d8d795ce6e5289f8f423b2194c61693fce7
Markus Uhlin <markus.uhlin@bredband.net>
2018-03-04 18:00:16 +0100

    build makefile

---
+++ build.mk
@@ -0,0 +1,19 @@
+OBJS=\
+	b64_decode.o\
+	b64_encode.o\
+	daemonize.o\
+	interpreter.o\
+	log.o\
+	main.o\
+	my_vasprintf.o\
+	network.o\
+	network-openssl.o\
+	settings.o\
+	sig.o\
+	strlcat.o\
+	strlcpy.o\
+	various.o\
+	wrapper.o
+
+enhanced-duc: $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $(OBJS) $(LDFLAGS)
