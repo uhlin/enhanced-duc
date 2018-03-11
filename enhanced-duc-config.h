bc1b85c8354f2a52cf9fde4dc24fec9a257528ee
Markus Uhlin <markus.uhlin@bredband.net>
2018-03-11 19:38:31 +0100

    HAVE_X509_CHECK_HOST

--- enhanced-duc-config.h
+++ enhanced-duc-config.h
@@ -7,4 +7,10 @@
 /* Set to 1 if your system has strlcat() */
 #define HAVE_STRLCAT 0
 
+/*
+ * Set to 0 if the OpenSSL implementation provided by your system
+ * doesn't have X509_check_host()
+ */
+#define HAVE_X509_CHECK_HOST 1
+
 #endif
