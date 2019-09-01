22ad63e39e1647e757cf474328ec2f8daac2737a
Markus Uhlin <markus.uhlin@bredband.net>
2019-09-01 18:19:53 +0200

    80 columns

--- base64.h
+++ base64.h
@@ -4,7 +4,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-int b64_decode(char const *src, uint8_t *target, size_t targsize);
-int b64_encode(uint8_t const *src, size_t srclength, char *target, size_t targsize);
+int	b64_decode(char const *src, uint8_t *target, size_t targsize);
+int	b64_encode(uint8_t const *src, size_t srclength, char *target,
+	    size_t targsize);
 
 #endif
