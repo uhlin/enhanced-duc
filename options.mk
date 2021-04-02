d84400f382f5251a820f1327d039f013f22e747d
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-02 12:40:54 +0200

    Updated file

--- options.mk
+++ options.mk
@@ -1,13 +1,14 @@
-CC=cc
-CFLAGS=\
-	-D_POSIX_C_SOURCE=200809L\
+# options.mk
+
+CC = cc
+CFLAGS = -D_POSIX_C_SOURCE=200809L\
 	-O2\
 	-Wall\
 	-pipe\
-	-std=c99
-CXX=c++
-CXXCLAGS=
-LDFLAGS=\
-	-lcrypto\
-	-lssl
-RM?=rm -f
+	-std=c11
+
+CXX = c++
+CXXCLAGS =
+
+LDFLAGS = -lcrypto -lssl
+RM ?= rm -f
