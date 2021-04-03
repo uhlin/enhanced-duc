95b2769613e2d2bc74b60d896c927481452a1289
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-03 22:17:22 +0200

    Fixed wrapper.h

--- wrapper.h
+++ wrapper.h
@@ -5,17 +5,19 @@
 
 #include <stdlib.h>
 
-char *strdup_printf (const char *format, ...) PRINTFLIKE(1);
-char *xstrdup       (const char *string);
-void *xcalloc       (size_t elt_count, size_t elt_size);
-void *xmalloc       (size_t);
-void *xrealloc      (void *ptr, size_t newSize);
+__DUC_BEGIN_DECLS
+char	*strdup_printf(const char *, ...) PRINTFLIKE(1);
+char	*xstrdup(const char *);
+void	*xcalloc(size_t elt_count, size_t elt_size);
+void	*xmalloc(size_t);
+void	*xrealloc(void *ptr, size_t newSize);
+__DUC_END_DECLS
 
 static inline void
 free_not_null(void *ptr)
 {
-    if (ptr != NULL)
-	free(ptr);
+	if (ptr != NULL)
+		free(ptr);
 }
 
 #endif
