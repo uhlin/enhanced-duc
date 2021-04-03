de25bee80439a03e60f4e6364fbb9487ce8709e9
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-04 01:32:02 +0200

    Fixed settings.h

--- settings.h
+++ settings.h
@@ -4,23 +4,23 @@
 #include "interpreter.h"
 
 struct integer_context {
-    char	*setting_name;
-    long int	 lo_limit;
-    long int	 hi_limit;
-    long int	 fallback_val;
+	char *setting_name;
+	long int lo_limit;
+	long int hi_limit;
+	long int fallback_val;
 };
 
+__DUC_BEGIN_DECLS
 extern bool g_conf_read;
 
-bool		 setting_bool(const char *setting_name,
-		     const bool fallback_val);
-char		*get_answer(const char *desc, enum setting_type,
-		     const char *defaultAnswer);
-const char	*setting(const char *setting_name);
+bool		 setting_bool(const char *, const bool);
+char		*get_answer(const char *, enum setting_type, const char *);
+const char	*setting(const char *);
 long int	 setting_integer(const struct integer_context *);
 void		 check_some_settings_strictly(void);
-void		 create_config_file(const char *path);
+void		 create_config_file(const char *);
 void		 destroy_config_custom_values(void);
-void		 read_config_file(const char *path);
+void		 read_config_file(const char *);
+__DUC_END_DECLS
 
 #endif
