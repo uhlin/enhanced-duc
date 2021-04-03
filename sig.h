83d695790fb28ec9f4839b01b0db72c960d80f0a
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-04 00:01:54 +0200

    Fixed sig.h

--- sig.h
+++ sig.h
@@ -1,7 +1,11 @@
 #ifndef GUARD_SIG_H
 #define GUARD_SIG_H
 
-int	sighand_init     (void);
-void	program_clean_up (void);
+#include "ducdef.h"
+
+__DUC_BEGIN_DECLS
+int	sighand_init(void);
+void	program_clean_up(void);
+__DUC_END_DECLS
 
 #endif
