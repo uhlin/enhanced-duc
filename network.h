681a069f68f097634165179a847f9ee15b541c66
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-05 13:52:26 +0200

    Fixed network.h

--- network.h
+++ network.h
@@ -7,41 +7,47 @@
 #include "ducdef.h"
 
 typedef enum {
-    IP_HAS_CHANGED,
-    IP_NO_CHANGE
+	IP_HAS_CHANGED,
+	IP_NO_CHANGE
 } ip_chg_t;
 
 typedef enum {
-    HOSTNAME_MATCH,
-    HOSTNAME_MISMATCH
+	HOSTNAME_MATCH,
+	HOSTNAME_MISMATCH
 } chkhost_res_t;
 
-typedef int (*NET_SEND_FUNCPTR)(const char *, ...);
+typedef int (*NET_SEND_FUNCPTR)(const char *, ...) PRINTFLIKE(1);
 typedef int (*NET_RECV_FUNCPTR)(char *, size_t);
 
+__DUC_BEGIN_DECLS
 extern NET_SEND_FUNCPTR net_send;
 extern NET_RECV_FUNCPTR net_recv;
 
 extern int g_socket;
 
-int		net_connect(void);
-void		net_disconnect(void);
+/* network.c */
+int	 net_connect(void);
+void	 net_disconnect(void);
 
-int		net_send_plain(const char *fmt, ...) PRINTFLIKE(1);
-int		net_recv_plain(char *recvbuf, size_t recvbuf_size);
-ip_chg_t	net_check_for_ip_change(void);
+int	 net_send_plain(const char *, ...) PRINTFLIKE(1);
+int	 net_recv_plain(char *, size_t);
 
-void		net_init(void);
-void		net_deinit(void);
+ip_chg_t net_check_for_ip_change(void);
 
-chkhost_res_t	net_ssl_check_hostname(const char *host, unsigned int flags);
-int		net_ssl_send(const char *fmt, ...) PRINTFLIKE(1);
-int		net_ssl_recv(char *recvbuf, size_t recvbuf_size);
+void	 net_init(void);
+void	 net_deinit(void);
 
-int		net_ssl_begin(void);
-void		net_ssl_end(void);
+/* network-openssl.c */
+chkhost_res_t net_ssl_check_hostname(const char *, unsigned int);
 
-void		net_ssl_init(void);
-void		net_ssl_deinit(void);
+int	 net_ssl_send(const char *, ...) PRINTFLIKE(1);
+int	 net_ssl_recv(char *, size_t);
+
+int	 net_ssl_begin(void);
+void	 net_ssl_end(void);
+
+void	 net_ssl_init(void);
+void	 net_ssl_deinit(void);
+__DUC_END_DECLS
 
 #endif
