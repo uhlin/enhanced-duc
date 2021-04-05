e182513bc408ef4f695dd463dc25959ec1533d39
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-05 16:36:25 +0200

    Fixed interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -7,31 +7,31 @@
 #include "ducdef.h"
 
 enum setting_type {
-    TYPE_BOOLEAN,
-    TYPE_INTEGER,
-    TYPE_STRING
+	TYPE_BOOLEAN,
+	TYPE_INTEGER,
+	TYPE_STRING
 };
 
 typedef bool (*Interpreter_vFunc)(const char *);
 typedef int (*Interpreter_instFunc)(const char *, const char *);
 
 struct Interpreter_in {
-    char			*path;
-    char			*line;
-    long int			 line_num;
-    Interpreter_vFunc		 validator_func;
-    Interpreter_instFunc	 install_func;
+	char *path;
+	char *line;
+	long int line_num;
+	Interpreter_vFunc validator_func;
+	Interpreter_instFunc install_func;
 };
 
-__BEGIN_DECLS
-void Interpreter(const struct Interpreter_in *) /* PTR_ARGS_NONNULL */;
-__END_DECLS
+__DUC_BEGIN_DECLS
+void	Interpreter(const struct Interpreter_in *);
+__DUC_END_DECLS
 
 static inline void
 adv_while_isspace(const char **ptr)
 {
-    while (isspace(**ptr))
-	(*ptr)++;
+	while (isspace(**ptr))
+		(*ptr)++;
 }
 
 #endif
