04727db3175c901fe6fe530b405ad1132319b22a
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-05 16:08:44 +0200

    Fixed log.h

--- log.h
+++ log.h
@@ -7,41 +7,38 @@
 
 #include "ducdef.h"
 
-__BEGIN_DECLS
-extern bool g_log_to_syslog;
-extern bool g_debug_mode;
-__END_DECLS
-
 typedef enum {
-    REDIR_OK,
-    REDIR_STDERR_FAIL,
-    REDIR_STDIN_FAIL,
-    REDIR_STDOUT_FAIL
+	REDIR_OK,
+	REDIR_STDERR_FAIL,
+	REDIR_STDIN_FAIL,
+	REDIR_STDOUT_FAIL
 } redir_res_t;
+
+__DUC_BEGIN_DECLS
+extern bool	 g_log_to_syslog;
+extern bool	 g_debug_mode;
 
 /*lint -sem(fatal, r_no) */
 
-__BEGIN_DECLS
-redir_res_t	redirect_standard_streams(void);
-__dead void	fatal(int code, const char *, ...) PRINTFLIKE(2);
+redir_res_t	 redirect_standard_streams(void);
+__dead void	 fatal(int, const char *, ...) PRINTFLIKE(2);
 
-void		log_debug(const char *, ...) PRINTFLIKE(1);
-void		log_init(void);
-void		log_msg(const char *, ...) PRINTFLIKE(1);
-void		log_warn(int code, const char *, ...) PRINTFLIKE(2);
-__END_DECLS
+void	 log_debug(const char *, ...) PRINTFLIKE(1);
+void	 log_init(void);
+void	 log_msg(const char *, ...) PRINTFLIKE(1);
+void	 log_warn(int, const char *, ...) PRINTFLIKE(2);
+__DUC_END_DECLS
 
 static inline void
-log_assert_arg_nonnull(
-    const char *in_func,
-    const char *arg_name,
-    const void *arg)
+log_assert_arg_nonnull(const char *in_func, const char *arg_name,
+		       const void *arg)
 {
-    if (arg == NULL) {
-	fatal(EINVAL, "In %s: Argument \"%s\" is null  --  assertion failed!",
-	      in_func  ? in_func  : "(no in_func)",
-	      arg_name ? arg_name : "(no arg_name)");
-    }
+	if (arg == NULL) {
+		fatal(EINVAL, "In %s: Argument \"%s\" is null  --  "
+		    "assertion failed!",
+		    (in_func ? in_func : "(no in_func)"),
+		    (arg_name ? arg_name : "(no arg_name)"));
+	}
 }
 
 #endif
