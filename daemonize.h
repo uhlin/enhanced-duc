7e2e029dbde2dceb4d5268aa6fad28a8a4902679
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-05 22:15:28 +0200

    Added daemonize.h

---
+++ daemonize.h
@@ -0,0 +1,17 @@
+#ifndef DAEMONIZE_H
+#define DAEMONIZE_H
+
+#include "ducdef.h"
+
+#define FORK_FAILED -1
+#define OBTAIN_LOCK_ERR -1
+#define VALUE_CHILD_PROCESS 0
+
+__DUC_BEGIN_DECLS
+extern const char	g_lockfile_path[];
+extern int		g_lockfile_fd;
+
+void	daemonize(void);
+__DUC_END_DECLS
+
+#endif
