0ab19a55f07abb387f6e33fdf4a6ed7c1b6825cb
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-07 17:11:08 +0200

    Fixed main.h

--- main.h
+++ main.h
@@ -3,39 +3,39 @@
 
 #include <stdbool.h>
 
+#include "ducdef.h"
+
 #define DUC_PATH_MAX			500	/* Max bytes in a pathname. */
 #define DUC_PERMITTED_HOSTS_LIMIT	10	/* Qty of hostnames allowed. */
 #define UID_SUPER_USER			0
 
+struct program_options {
+	bool want_usage;
+	bool want_create_config_file;
+	bool want_debug;
+	bool want_update_once;
+	bool want_daemon;
+};
+
+typedef enum {
+	CODE_GOOD,
+	CODE_NOCHG,
+	CODE_NOHOST,
+	CODE_BADAUTH,
+	CODE_BADAGENT,
+	CODE_NOTDONATOR,
+	CODE_ABUSE,
+	CODE_EMERG,
+	CODE_UNKNOWN
+} response_code_t;
+
+__DUC_BEGIN_DECLS
 extern const char g_programName[];
 extern const char g_programVersion[];
 extern const char g_programAuthor[];
 extern const char g_maintainerEmail[];
 
 extern char g_last_ip_addr[100];
-
-struct program_options {
-    bool want_usage;
-    bool want_create_config_file;
-    bool want_debug;
-    bool want_update_once;
-    bool want_daemon;
-};
-
-typedef enum {
-    CODE_GOOD,
-    CODE_NOCHG,
-    CODE_NOHOST,
-    CODE_BADAUTH,
-    CODE_BADAGENT,
-    CODE_NOTDONATOR,
-    CODE_ABUSE,
-    CODE_EMERG,
-    CODE_UNKNOWN
-} response_code_t;
-
-#if __OpenBSD__ && !defined(_BSD_SOURCE)
-int pledge(const char *, const char *);
-#endif
+__DUC_END_DECLS
 
 #endif
