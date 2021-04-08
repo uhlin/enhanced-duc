c3daf513a05ec15b0da5338c3f0aad8c6c51da97
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-08 15:55:28 +0200

    Fixed xstrdup.c

--- tests/xstrdup.c
+++ tests/xstrdup.c
@@ -10,32 +10,36 @@
 static void
 canDuplicateString_test1(void **state)
 {
-    const char string[] = "Hello world! Unit testing in progress...";
-    char *result = NULL;
+	(void) state;
 
-    result = xstrdup(string);
-    assert_string_equal(result, string);
-    free(result);
+	const char string[] = "Hello World! Unit testing in progress...";
+	char *result = NULL;
+
+	result = xstrdup(string);
+	assert_string_equal(result, string);
+	free(result);
 }
 
 static void
 canDuplicateString_test2(void **state)
 {
-    const char string[] = "Hello world! My name is Markus.\r\n";
-    char *result = NULL;
+	(void) state;
 
-    result = xstrdup(string);
-    assert_string_equal(result, string);
-    free(result);
+	const char string[] = "Hello world! My name is Markus.\r\n";
+	char *result = NULL;
+
+	result = xstrdup(string);
+	assert_string_equal(result, string);
+	free(result);
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(canDuplicateString_test1),
-	cmocka_unit_test(canDuplicateString_test2),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(canDuplicateString_test1),
+		cmocka_unit_test(canDuplicateString_test2),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
