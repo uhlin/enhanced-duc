17659f771bf46a1807afe9080cae0d05c3e0bb9d
Markus Uhlin <markus.uhlin@bredband.net>
2021-04-08 16:29:14 +0200

    Fixed is_numeric.c

--- tests/is_numeric.c
+++ tests/is_numeric.c
@@ -8,64 +8,64 @@
 static void
 returnsFalse_test1(void **state)
 {
-    assert_false(is_numeric("0123456789 "));
+	assert_false(is_numeric("0123456789 "));
 }
 
 static void
 returnsFalse_test2(void **state)
 {
-    assert_false(is_numeric(" 0123456789"));
+	assert_false(is_numeric(" 0123456789"));
 }
 
 static void
 returnsFalse_test3(void **state)
 {
-    assert_false(is_numeric("123abc"));
+	assert_false(is_numeric("123abc"));
 }
 
 static void
 returnsFalseIfStringIsNull(void **state)
 {
-    assert_false(is_numeric(NULL));
+	assert_false(is_numeric(NULL));
 }
 
 static void
 returnsFalseIfStringIsEmpty(void **state)
 {
-    assert_false(is_numeric(""));
+	assert_false(is_numeric(""));
 }
 
 static void
 returnsTrue_test1(void **state)
 {
-    assert_true(is_numeric("0123456789"));
+	assert_true(is_numeric("0123456789"));
 }
 
 static void
 returnsTrue_test2(void **state)
 {
-    assert_true(is_numeric("122333"));
+	assert_true(is_numeric("122333"));
 }
 
 static void
 returnsTrue_test3(void **state)
 {
-    assert_true(is_numeric("0"));
+	assert_true(is_numeric("0"));
 }
 
 int
 main(void)
 {
-    const struct CMUnitTest tests[] = {
-	cmocka_unit_test(returnsFalse_test1),
-	cmocka_unit_test(returnsFalse_test2),
-	cmocka_unit_test(returnsFalse_test3),
-	cmocka_unit_test(returnsFalseIfStringIsNull),
-	cmocka_unit_test(returnsFalseIfStringIsEmpty),
-	cmocka_unit_test(returnsTrue_test1),
-	cmocka_unit_test(returnsTrue_test2),
-	cmocka_unit_test(returnsTrue_test3),
-    };
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(returnsFalse_test1),
+		cmocka_unit_test(returnsFalse_test2),
+		cmocka_unit_test(returnsFalse_test3),
+		cmocka_unit_test(returnsFalseIfStringIsNull),
+		cmocka_unit_test(returnsFalseIfStringIsEmpty),
+		cmocka_unit_test(returnsTrue_test1),
+		cmocka_unit_test(returnsTrue_test2),
+		cmocka_unit_test(returnsTrue_test3),
+	};
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+	return cmocka_run_group_tests(tests, NULL, NULL);
 }
