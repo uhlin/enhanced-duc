49bc0c3b8105fb106c88606795da56920ceaba1d
Markus Uhlin <markus.uhlin@bredband.net>
2023-01-10 11:02:56 +0100

    Added missing include

--- source/interpreter.cpp
+++ source/interpreter.cpp
@@ -20,6 +20,7 @@
 #include "interpreter.h"
 #include "log.h"
 #include "various.h"
+#include "wrapper.h"
 
 const char g_fgets_nullret_err1[] = "error: fgets() returned null and the "
     "error indicator is set";
