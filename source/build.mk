68b2a0d4a52dd2dd97e6a99d48b4586af1899b15
Markus Uhlin <markus.uhlin@bredband.net>
2023-01-10 09:23:15 +0100

    Added and made use of 'SRC_DIR'

--- source/build.mk
+++ source/build.mk
@@ -1,18 +1,20 @@
-OBJS = b64_decode.o\
-	b64_encode.o\
-	daemonize.o\
-	interpreter.o\
-	log.o\
-	main.o\
-	my_vasprintf.o\
-	network.o\
-	network-openssl.o\
-	settings.o\
-	sig.o\
-	strlcat.o\
-	strlcpy.o\
-	various.o\
-	wrapper.o
+SRC_DIR := source/
+
+OBJS = $(SRC_DIR)b64_decode.o\
+	$(SRC_DIR)b64_encode.o\
+	$(SRC_DIR)daemonize.o\
+	$(SRC_DIR)interpreter.o\
+	$(SRC_DIR)log.o\
+	$(SRC_DIR)main.o\
+	$(SRC_DIR)my_vasprintf.o\
+	$(SRC_DIR)network.o\
+	$(SRC_DIR)network-openssl.o\
+	$(SRC_DIR)settings.o\
+	$(SRC_DIR)sig.o\
+	$(SRC_DIR)strlcat.o\
+	$(SRC_DIR)strlcpy.o\
+	$(SRC_DIR)various.o\
+	$(SRC_DIR)wrapper.o
 
 enhanced-duc: $(OBJS)
 	$(E) "  LINK    " $@
