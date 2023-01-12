84ac752b3ead4916d6c9ba8d65f581e23ae01fb5
Markus Uhlin <markus.uhlin@bredband.net>
2023-01-12 14:43:08 +0100

    OBJS: added terminate.o

--- source/build.mk
+++ source/build.mk
@@ -7,12 +7,13 @@ OBJS = $(SRC_DIR)b64_decode.o\
 	$(SRC_DIR)log.o\
 	$(SRC_DIR)main.o\
 	$(SRC_DIR)my_vasprintf.o\
-	$(SRC_DIR)network.o\
 	$(SRC_DIR)network-openssl.o\
+	$(SRC_DIR)network.o\
 	$(SRC_DIR)settings.o\
 	$(SRC_DIR)sig.o\
 	$(SRC_DIR)strlcat.o\
 	$(SRC_DIR)strlcpy.o\
+	$(SRC_DIR)terminate.o\
 	$(SRC_DIR)various.o\
 	$(SRC_DIR)wrapper.o
 
