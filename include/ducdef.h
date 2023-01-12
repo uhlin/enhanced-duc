a76a3f74484ea3b4ace29eba438f205ce60e5edf
Markus Uhlin <markus.uhlin@bredband.net>
2023-01-12 15:02:29 +0100

    Added a license

--- include/ducdef.h
+++ include/ducdef.h
@@ -1,5 +1,20 @@
 #ifndef DUC_DEFINITIONS_H
 #define DUC_DEFINITIONS_H
+/* Copyright (c) 2021-2023 Markus Uhlin <markus.uhlin@bredband.net>
+   All rights reserved.
+
+   Permission to use, copy, modify, and distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+   AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+   DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+   PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+   TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+   PERFORMANCE OF THIS SOFTWARE. */
 
 #define BZERO(buf, sz) ((void) memset(buf, 0, sz))
 
